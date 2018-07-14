bd481191ab5306d7ef5954d17687f3ac2975a371
fjodor-rybakov <fedryb@mail.ru>
2018-07-14 16:36:45 +0300

    add search form

--- src/js/Header.js
+++ src/js/Header.js
@@ -7,16 +7,22 @@ class Header extends Component {
 			<nav className="navbar navbar-default">
 				<div className="container">
 					<div className="navbar-header">
-						<Link to='/' className="navbar-brand">{this.props.name}</Link>
+						<Link to="/" className="navbar-brand">{this.props.name}</Link>
 					</div>
 
 					<div>
 						<ul className="nav navbar-nav">
-							<li><Link to='/'>Главная</Link></li>
-							<li><Link to='/books/showPage/1'>Просмотреть доступные книги</Link></li>
-							<li><Link to='/'>Забронировать книгу</Link></li>
-							<li><Link to='/'>Добавить книгу</Link></li>
-							<li><Link to='/'>Удалить книгу</Link></li>
+							<li><Link to="/">Главная</Link></li>
+							<li><Link to="/books/showPage/1">Просмотреть доступные книги</Link></li>
+							<li><Link to="/">Забронировать книгу</Link></li>
+							<li><Link to="/">Добавить книгу</Link></li>
+							<li><Link to="/">Удалить книгу</Link></li>
+							<from className="navbar-form navbar-right" role="search">
+								<div className="from-group">
+									<input type="text" className="form-control" placeholder="Имя книги"/>
+									<button type="submit" className="btn btn-default">Поиск</button>
+								</div>
+							</from>
 						</ul>
 					</div>
 				</div>
